7221a397363a6d3ee689f78995014e44e2afc06f
Saioaa <saioa.losarcos@materialight.com>
2026-09-21 14:42:08 +0200

    comprobar conexión m-duino

--- programa/mduino.py
+++ programa/mduino.py
@@ -50,6 +50,7 @@ class ClienteMDuino(QObject):
         self._hilo = None
         self._activo = False
         self._intentos = 0
+        self._ultimo_por_clave = {}   # ultimo valor recibido por prefijo (SETA, REF...), para la traza
 
     # ----- conexion -----
 
@@ -95,6 +96,7 @@ class ClienteMDuino(QObject):
                     sock.close()
             if self.sock is not None:
                 self.sock = None
+                self._ultimo_por_clave.clear()
                 self.conexion_cambiada.emit(False)
             if self._activo:
                 time.sleep(RETARDO_RECONEXION)
@@ -115,6 +117,11 @@ class ClienteMDuino(QObject):
     def _procesar_linea(self, linea):
         if not linea:
             return
+        # traza en consola solo cuando cambia el valor (el M-Duino repite SETA/REF cada segundo)
+        clave = linea.split(":", 1)[0]
+        if self._ultimo_por_clave.get(clave) != linea:
+            self._ultimo_por_clave[clave] = linea
+            print(f"[MDUINO] <- {linea}")
         self.linea_recibida.emit(linea)
         # SETA DESACTIVADA: el M-Duino aun no esta conectado fisicamente.
         # Descomentar cuando se pueda probar con la seta real.
